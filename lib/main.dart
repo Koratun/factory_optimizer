49b92a3dbd827e42995941eef8c6139cbfee7480
Kody Mitchell <kody.mitchell@involio.com>
2022-09-03 14:14:37 -0700

    Completed base app setup with ability to add profile with name and image

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+
+const String rootDir = "./Factory Optimizer";
 
 void main() {
   runApp(const MyApp());
@@ -21,46 +24,74 @@ class MyApp extends StatelessWidget {
         textTheme: Typography.whiteRedmond,
         primarySwatch: Colors.blue,
       ),
-      home: Center(
-        child: ListView(
-          children: [
-            Text(
-              "Factory Optimizer",
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            Text(
-              "Select Game Profile:",
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            SizedBox(
-              width: 400,
-              height: 100,
-              child: ListTile(
-                leading: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  "Add New Profile",
+      home: Scaffold(
+        body: Builder(
+          builder: (context) => Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Factory Optimizer",
+                  textWidthBasis: TextWidthBasis.longestLine,
                   style: Theme.of(context).textTheme.displaySmall,
                 ),
-                onTap: () async {
-                  showDialog(context: context, builder: (context){
-                    Dialog(alignment: Alignment.center, child: TextField(autofocus: true, ),)
-                  });
-
-
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles(type: FileType.image);
-                  if (result != null) {
-                    File file = File(result.files.first.path!);
-
-                  }
-                },
-              ),
+                Text(
+                  "Select Game Profile:",
+                  textWidthBasis: TextWidthBasis.longestLine,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                newProfileWidget(context),
+              ],
             ),
-          ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget newProfileWidget(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      child: ListTile(
+        leading: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        title: Text(
+          "Add New Profile",
+          textWidthBasis: TextWidthBasis.longestLine,
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        onTap: () async {
+          String? name = await showDialog<String>(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 200,
+                    child: TextField(
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter game name",
+                        fillColor: Colors.grey.shade700,
+                      ),
+                      onSubmitted: (s) => Navigator.pop(context, s),
+                    ),
+                  ),
+                );
+              });
+
+          if (name != null) {
+            FilePickerResult? result =
+                await FilePicker.platform.pickFiles(type: FileType.image);
+            if (result != null) {
+              File file = File(result.files.first.path!);
+              Directory("$rootDir/$name").createSync(recursive: true);
+              file.copySync("$rootDir/$name/profile${extension(file.path)}");
+            }
+          }
+        },
       ),
     );
   }
